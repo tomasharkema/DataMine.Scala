2a9c10b9e5c44c10882a3fab37dd85a9f9297bd5
Tomas Harkema <tomas@harkema.in>
2016-02-27 14:27:05 +0100

    added concurrency

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -3,6 +3,9 @@ package main
 import Helpers._
 import com.typesafe.scalalogging.LazyLogging
 
+import scala.concurrent.Future
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main extends LazyLogging {
 
@@ -12,52 +15,63 @@ object Main extends LazyLogging {
 
   def main(args: Array[String]) {
 
-    println("Load file")
-    val stream = getClass.getResourceAsStream("/klachtendumpgemeente.csv")
-    println("Load loaded. Read it.")
-    val lines = scala.io.Source.fromInputStream(stream).getLines
-    println("Got lines. Parsing it")
-    val rows = lines.map(SeparatorIterator(_, ";")) drop 1 map { _ map replaceIrregularities }
-    println("Parsed lines. Crunching it")
+    logger.info("Load file")
+    val stream =  Future { getClass.getResourceAsStream("/klachtendumpgemeente.csv") }
+    val lines = stream.flatMap { stream =>
+      logger.info("Load loaded. Read it.")
+      Future { scala.io.Source.fromInputStream(stream).getLines }
+    }
+    val rows = lines.flatMap { lines =>
+      logger.info("Got lines. Parsing it")
+      Future { lines.map(SeparatorIterator(_, ";")) drop 1 map { _ map replaceIrregularities } }
+    }
+    logger.info("Parsed lines. Crunching it")
 
-    val categoriesAndInput = rows
-      .map { el =>
+    val categoriesAndInput = rows.map { rows =>
+      rows.map { el =>
         new {
           val key = el.take(1).toStream.head.toLowerCase
-          val value = el// slice(1, 4)
+          val value = el // slice(1, 4)
         }
-      }
-      .toStream.groupByKeyAndValue(_.key)(_.value.toStream)
-
-
-    val categoriesAndWords = categoriesAndInput
-      .map { el =>
-        (el._1, el._2
-          .map(_.replaceAll("\\.", " ")
-            .replaceAll("\\,", " ")
-            .replaceAll("\n", " "))
-          .flatMap( message =>
-            SeparatorIterator(message, " ")
-              .filter(_ != "")
-              .toStream
-              .map(word => word.toLowerCase)
-              .groupBy(word => word)
-              .toStream
-              .map(k => (k._1, k._2.toSeq.size))
-              .sortBy(_._2)
-              .reverse
-         ))
-      }
-
-    categoriesAndWords.foreach { l =>
-      logger.debug("-------")
-      logger.debug("ONDERWERP: " + l._1)
-      logger.debug("top 5 words")
-      l._2.toSeq take 5 foreach( el => logger.debug(el._1 + " keer: " + el._2))
+      }.toStream.groupByKeyAndValue(_.key)(_.value.toStream)
     }
 
 
-    true
+    val categoriesAndWords = categoriesAndInput.map { categoriesAndInput =>
+      categoriesAndInput.map { el =>
+        Future {
+          (el._1, el._2
+            .map(_.replaceAll("\\.", " ")
+              .replaceAll("\\,", " ")
+              .replaceAll("\n", " "))
+            .flatMap { message =>
+              SeparatorIterator(message, " ")
+                .filter(_ != "")
+                .toStream
+                .map(word => word.toLowerCase)
+                .groupBy(word => word)
+                .toStream
+                .map(k => (k._1, k._2.toSeq.size))
+                .sortBy(_._2)
+                .reverse
+            })
+        }
+      }
+    }
+
+    val loggerSync = new Object
+
+    categoriesAndWords.map { categoriesAndWords => categoriesAndWords.foreach { l =>
+      l.map { l =>
+        loggerSync.synchronized {
+          logger.debug("-------")
+          logger.debug("ONDERWERP: " + l._1)
+          logger.debug("top 5 words")
+          l._2.toSeq take 5 foreach (el => logger.debug(el._1 + " keer: " + el._2))
+          logger.debug("-------")
+        }
+      }
+    }}
   }
 
 }
