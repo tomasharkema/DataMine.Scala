7d8710521aa1874c31e387a622db6f68acb7714a
Tomas Harkema <tomas@harkema.in>
2016-02-27 14:08:01 +0100

    add triple tests

--- src/test/scala/main/HelpersStreamTest.scala
+++ src/test/scala/main/HelpersStreamTest.scala
@@ -28,6 +28,7 @@ class HelpersStreamTest extends Specification {
 
       grouped should haveKeys("N", "J", "A", "M", "W", "Z")
     }
+
     "be grouped by first character and double items" in {
       val stream = Stream("Aap", "Noot", "Mies", "Wim", "Zus", "Jet", "Aap", "Noot", "Mies", "Wim", "Zus", "Jet")
 
@@ -43,6 +44,25 @@ class HelpersStreamTest extends Specification {
       grouped should havePair("W" -> List("Wim", "Wim"))
       grouped should havePair("Z" -> List("Zus", "Zus"))
       grouped should havePair("J" -> List("Jet", "Jet"))
+
+      grouped should haveKeys("N", "J", "A", "M", "W", "Z")
+    }
+
+    "be grouped by first character and triple items" in {
+      val stream = Stream("Aap", "Noot", "Mies", "Wim", "Zus", "Jet", "Aap", "Noot", "Mies", "Wim", "Zus", "Jet", "Aap", "Noot", "Mies", "Wim", "Zus", "Jet")
+
+      val grouped = stream
+        .groupByKeyAndValue(key => key.head.toString)(value => Stream(value))
+        .map(el => (el._1, el._2.toList))
+
+      grouped should have size 6
+
+      grouped should havePair("A" -> List("Aap", "Aap", "Aap"))
+      grouped should havePair("N" -> List("Noot", "Noot", "Noot"))
+      grouped should havePair("M" -> List("Mies", "Mies", "Mies"))
+      grouped should havePair("W" -> List("Wim", "Wim", "Wim"))
+      grouped should havePair("Z" -> List("Zus", "Zus", "Zus"))
+      grouped should havePair("J" -> List("Jet", "Jet", "Jet"))
 
       grouped should haveKeys("N", "J", "A", "M", "W", "Z")
     }
